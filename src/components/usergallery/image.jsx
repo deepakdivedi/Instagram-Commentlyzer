14f54d0a55680465bf5faaf7dbaeb1c7c1e72f4b
Shubham Kamath <dev.shubhaem@gmail.com>
2019-01-27 22:20:39 +0530

    Added full screen for images

--- src/components/usergallery/image.jsx
+++ src/components/usergallery/image.jsx
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 class Image extends Component {
   render(){
     return(
-      <div className="d-flex justify-content-center" style = {{width:"240px"}}>
-        <img style = {{height: "240px", width:"auto"}} className="" alt ="" src={this.props.url}  />
+      <div className="d-flex justify-content-center">
+        <img style = {{height: "auto", width:"auto"}} className="" alt ="" src={this.props.url}  onClick={()=>{this.props.allowFullScreen(this.props.index)}}/>
       </div>
     );
   }
--- src/components/usergallery/usergallery.jsx
+++ src/components/usergallery/usergallery.jsx
@@ -8,8 +8,11 @@ class UserGallery extends Component {
     this.state={
       imageObject : false,
       max_id : "",
-      hmp : false
+      hmp : false,
+      fullscreen : false,
+      index : ""
     }
+    this.toggle_full_screen = this.toggle_full_screen.bind(this);
   }
   componentDidMount = () => {
     axios.put(this.props.address+'/userfeed',{'max_id':this.state.max_id}).then((response) => {
@@ -18,28 +21,52 @@ class UserGallery extends Component {
         max_id : response.data.max_id,
         hmp : response.data.hmp
       })
+      console.log(this.state.imageObject);
     });
   }
+
+  toggle_full_screen = (index) => {
+    var toggle_value = false
+    if(!this.state.fullscreen){
+      toggle_value = true
+    }
+    this.setState({
+      fullscreen : toggle_value,
+      index : index
+    })
+  }
+
   render(){
-    return(
-        <div>
+    if(this.state.fullscreen){
+      return(
+        <Image
+      url = {this.state.imageObject[this.state.index]['image_versions2']['candidates'][0]['url']}
+      allowFullScreen = {this.toggle_full_screen}
+      fullscreen = {this.state.fullscreen}></Image>
+  );
+    }else{
+      return(
           <div>
-            <div className="row" style = {{overflowY: 'scroll', height: '80vh', marginTop: "20px", marginBottom: "20px", width:"100%"}}>
-              {Object.keys(this.state.imageObject).map((i) => {
-                return (
-                  <div className="m-auto">
-                    <div className="card m-2">
-                    <Image 
-                    key = {i}
-                    url={this.state.imageObject[i]['image_versions2']['candidates'][1]['url']}></Image>
-                  </div>
-                  </div>
-                )
-              })}
+            <div>
+              <div className="row" style = {{overflowY: 'scroll', height: '80vh', marginTop: "20px", marginBottom: "20px", width:"100%"}}>
+                {Object.keys(this.state.imageObject).map((i) => {
+                  return (
+                    <div className="m-auto">
+                      <div className="card m-2">
+                      <Image
+                      key = {i}
+                      url = {this.state.imageObject[i]['image_versions2']['candidates'][1]['url']}
+                      allowFullScreen = {this.toggle_full_screen}
+                      index = {i}></Image>
+                    </div>
+                    </div>
+                  )
+                })}
+              </div>
             </div>
           </div>
-        </div>
-    );
+      );
+    }
   }
 }
 
