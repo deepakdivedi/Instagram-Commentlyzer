e74b41720497ef98a08e2a215a115fe8343a7974
Shubham Kamath <dev.shubhaem@gmail.com>
2019-01-30 22:54:34 +0530

    Updates

---
+++ precache-manifest.a9f6b164831b384d7cb5bd4fbdf2d3f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c9b708d7d6b3e409b6d",
+    "url": "/Commentlyzer/static/js/runtime~main.8c9b708d.js"
+  },
+  {
+    "revision": "7350a7d57f2ee5cd4215",
+    "url": "/Commentlyzer/static/js/main.7350a7d5.chunk.js"
+  },
+  {
+    "revision": "d32f67e8c1c3538f46fa",
+    "url": "/Commentlyzer/static/js/1.d32f67e8.chunk.js"
+  },
+  {
+    "revision": "7350a7d57f2ee5cd4215",
+    "url": "/Commentlyzer/static/css/main.eac3ed92.chunk.css"
+  },
+  {
+    "revision": "d32f67e8c1c3538f46fa",
+    "url": "/Commentlyzer/static/css/1.15aac153.chunk.css"
+  },
+  {
+    "revision": "f879a1a720fec32d6b463ed906394041",
+    "url": "/Commentlyzer/index.html"
+  }
+];
